e374e21815803676a43509ddeceb8d2ff0ef8af0
WingZer0o <mtmulch0191@outlook.com>
2024-04-20 18:45:42 -0400

    adding prisma testing connection

--- routes/user-auth/login-router.ts
+++ routes/user-auth/login-router.ts
@@ -1,10 +1,18 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
 import { LoginDto } from "../../models/user-auth/login-dto.ts";
+import { PrismaClient } from '../../generated/client/deno/edge.ts'
 
+const prisma = new PrismaClient();
 const router = new Router();
 
-router.post("/login", async (context) => {
+router.post("/register", async (context) => {
     const body: LoginDto = await context.request.body.json();
+    await prisma.user.create({
+        data: {
+          email: body.userName,
+          password: body.password
+        },
+      })
     context.response.body = {};
 });
 
