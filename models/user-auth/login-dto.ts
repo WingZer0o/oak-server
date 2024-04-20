83133c6ac8e93036a8df272fda0ff79a26c7e47f
Mike Mulchrone <mtmulch0191@outlook.com>
2024-04-20 15:48:59 -0400

    testing

---
+++ models/user-auth/login-dto.ts
@@ -0,0 +1,4 @@
+export class LoginDto {
+    public userName!: string;
+    public password!: string;
+}
--- routes/user-auth/login-router.ts
+++ routes/user-auth/login-router.ts
@@ -1,9 +1,11 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
+import { LoginDto } from "../../models/user-auth/login-dto.ts";
 
 const router = new Router();
 
-router.post("/", async (context) => {
-    
+router.post("/login", async (context) => {
+    const body: LoginDto = await context.request.body.json();
+    console.log(body);
 });
 
 export default router;
