67c63d22f4efb24996cdb6789eb0d30fd16d5048
WingZer0o <mtmulch0191@outlook.com>
2024-04-20 19:00:54 -0400

    renaming register router

--- routes/index.ts
+++ routes/index.ts
@@ -1,5 +1,5 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
-import loginRouter from "./user-auth/login-router.ts";
+import loginRouter from "./user-auth/register-router.ts";
 
 const indexRouter = new Router();
 indexRouter.use(loginRouter.routes());
