4645a5b73bef83eb1c5540bdcaa6f8726c63a4da
WingZer0o <mtmulch0191@outlook.com>
2024-04-21 01:03:22 -0400

    deno json

--- deps.ts
+++ deps.ts
@@ -1,5 +1,4 @@
-export { Application, Router } from "https://deno.land/x/oak@v16.0.0/mod.ts";
-export type { RouterContext } from "https://deno.land/x/oak@v16.0.0/router.ts";
-
-export { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
-export * as bcrypt from "https://deno.land/x/bcrypt@v0.3.0/mod.ts";
+export { Application, Router } from "oak";
+export type { RouterContext } from "oakRouter";
+export { oakCors } from "cors";
+export * as bcrypt from "bcrypt";
